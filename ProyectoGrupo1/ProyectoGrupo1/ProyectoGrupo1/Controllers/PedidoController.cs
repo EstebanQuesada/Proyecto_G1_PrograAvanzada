d2918194eeafced54ed52bd8da8a9836ac3e2143
MonserratRojas <114606974+MonserratRojas@users.noreply.github.com>
2025-07-24 15:24:37 -0600

    Proceso de compra

--- ProyectoGrupo1/ProyectoGrupo1/ProyectoGrupo1/Controllers/PedidoController.cs
+++ ProyectoGrupo1/ProyectoGrupo1/ProyectoGrupo1/Controllers/PedidoController.cs
@@ -4,6 +4,8 @@ using ProyectoGrupo1.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using System;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
 
 namespace ProyectoGrupo1.Controllers
 {
@@ -84,6 +86,75 @@ namespace ProyectoGrupo1.Controllers
                 model.ProductosPTC = _pedidoService.ObtenerProductosPTC();
                 return View(model);
             }
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            int? usuarioId = HttpContext.Session.GetInt32("UsuarioID");
+            if (usuarioId == null)
+                return RedirectToAction("Login", "Usuario");
+
+            // Obtener datos de usuario y dirección
+            var usuarioService = new ProyectoGrupo1.Services.UsuarioService(HttpContext.RequestServices.GetService<IConfiguration>());
+            var usuario = usuarioService.ObtenerPerfilCompleto(usuarioId.Value);
+
+            // Obtener carrito
+            var carritoService = new ProyectoGrupo1.Service.CarritoService(HttpContext.RequestServices.GetService<IConfiguration>());
+            var carrito = carritoService.ObtenerCarritoPorUsuario(usuarioId.Value);
+
+            ViewBag.Carrito = carrito;
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(string Nombre, string Apellido, string Direccion, string Ciudad, string Provincia, string CodigoPostal, string MetodoPago)
+        {
+            int? usuarioId = HttpContext.Session.GetInt32("UsuarioID");
+            if (usuarioId == null)
+                return RedirectToAction("Login", "Usuario");
+
+            // Actualizar datos de usuario y dirección
+            var usuarioService = new ProyectoGrupo1.Services.UsuarioService(HttpContext.RequestServices.GetService<IConfiguration>());
+            var usuario = usuarioService.ObtenerPerfilCompleto(usuarioId.Value);
+            usuario.Nombre = Nombre;
+            usuario.Apellido = Apellido;
+            usuario.Direccion = Direccion;
+            usuario.Ciudad = Ciudad;
+            usuario.Provincia = Provincia;
+            usuario.CodigoPostal = CodigoPostal;
+            usuarioService.ActualizarPerfilYDireccion(usuario);
+
+            // Obtener carrito
+            var carritoService = new ProyectoGrupo1.Service.CarritoService(HttpContext.RequestServices.GetService<IConfiguration>());
+            var carrito = carritoService.ObtenerCarritoPorUsuario(usuarioId.Value);
+
+            // Registrar pedido y detalles
+            var pedidoService = new ProyectoGrupo1.Services.PedidoService(HttpContext.RequestServices.GetService<IConfiguration>());
+            var nuevoPedido = new ProyectoGrupo1.Models.NuevoPedidoInputModel
+            {
+                EstadoID = 1, // Pendiente
+                Detalles = carrito.Detalles.Select(d => new ProyectoGrupo1.Models.NuevoDetallePedidoInputModel
+                {
+                    PTCID = d.PTCID,
+                    Cantidad = d.Cantidad,
+                    PrecioUnitario = d.PrecioUnitario
+                }).ToList()
+            };
+            pedidoService.CrearPedidoConDetalles(usuarioId.Value, nuevoPedido);
+
+            // Limpiar carrito
+            carritoService.VaciarCarrito(usuarioId.Value);
+
+            // Mostrar confirmación
+            TempData["Confirmacion"] = "¡Tu pedido ha sido registrado exitosamente!";
+            return RedirectToAction("Confirmacion");
+        }
+
+        public IActionResult Confirmacion()
+        {
+            ViewBag.Mensaje = TempData["Confirmacion"];
+            return View();
         }
     }
 }
